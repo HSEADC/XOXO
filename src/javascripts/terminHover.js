7f6e64d65e2465e73fad8fce9a7a13ffb48ea1fa
vika yamshikova <vikushayam@gmail.com>
2024-10-27 14:30:10 +0300

    add visible termin

--- src/javascripts/terminHover.js
+++ src/javascripts/terminHover.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   const word = document.querySelector('.highlighted-word')
+  const termin = document.querySelector('.termin')
   const content = document.querySelector('.article')
 
   word.addEventListener('mouseenter', () => {
-    content.classList.add('blur')
+    content.classList.add('blur'), termin.classList.add('visible_termin')
   })
   word.addEventListener('mouseleave', () => {
-    content.classList.remove('blur')
+    content.classList.remove('blur'), termin.classList.remove('visible_termin')
   })
 })
