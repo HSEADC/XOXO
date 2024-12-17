27b74e6e6bee035769f2a592e198121c70edd827
Yamshikova Victoriya <vikushayam@gmail.com>
2024-12-17 14:17:55 +0000

    add function

--- src/javascripts/burgerMenu.js
+++ src/javascripts/burgerMenu.js
@@ -1,15 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   const burgerMenu = document.querySelector('.burger-menu')
   const item = document.querySelector('.item-menu')
-  const content = document.querySelector('.menu-mobile')
 
   item.addEventListener('click', () => {
-    burgerMenu.classList.add('active')
+    burgerMenu.classList.toggle('active')
   })
-
-  if (burgerMenu.classList.has('active') == 1) {
-    item.addEventListener('click', () => {
-      burgerMenu.classList.remove('active')
-    })
-  }
 })
