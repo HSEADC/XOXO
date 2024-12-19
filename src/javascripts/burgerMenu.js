b426e04ca958db9c1ca9a84ebe712871ebfd952a
Vika <vikushayam@gmail.com>
2024-12-20 00:30:30 +0300

    add menu burger

--- src/javascripts/burgerMenu.js
+++ src/javascripts/burgerMenu.js
@@ -1,8 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
   const burgerMenu = document.querySelector('.burger-menu')
   const item = document.querySelector('.item-menu')
+  const nav = document.querySelector('.burger-nav')
+  const logoMenu = document.querySelector('.logo-menu')
+
 
   item.addEventListener('click', () => {
-    burgerMenu.classList.toggle('active')
+    burgerMenu.classList.toggle('active'), 
+    nav.classList.toggle('nav-active')
+  })
+  item.addEventListener('click', () => {
+      logoMenu.classList.toggle('logo-inactive')
   })
 })
