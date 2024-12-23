1c5d31c3bd8a0dad011a518d21ac8aa53400ac9d
Vika <vikushayam@gmail.com>
2024-12-23 19:57:35 +0300

    build

---
+++ docs/flipCard.js
@@ -0,0 +1,40 @@
+/******/ (() => { // webpackBootstrap
+document.addEventListener('DOMContentLoaded', function () {
+  var card = document.querySelector('.sleng_1');
+  var card2 = document.querySelector('.sleng_2');
+  var card3 = document.querySelector('.sleng_3');
+  var card4 = document.querySelector('.sleng_4');
+  var card5 = document.querySelector('.sleng_5');
+  var card6 = document.querySelector('.sleng_6');
+  var card7 = document.querySelector('.sleng_7');
+  var card8 = document.querySelector('.sleng_8');
+  card.addEventListener('click', function () {
+    card.classList.toggle('is-flipped');
+  });
+  card2.addEventListener('click', function () {
+    card2.classList.toggle('is-flipped');
+  });
+  card3.addEventListener('click', function () {
+    card3.classList.toggle('is-flipped');
+  });
+  card4.addEventListener('click', function () {
+    card4.classList.toggle('is-flipped');
+  });
+  card5.addEventListener('click', function () {
+    card5.classList.toggle('is-flipped');
+  });
+  card6.addEventListener('click', function () {
+    card6.classList.toggle('is-flipped');
+  });
+  card7.addEventListener('click', function () {
+    card7.classList.toggle('is-flipped');
+  });
+  card2.addEventListener('click', function () {
+    card2.classList.toggle('is-flipped');
+  });
+  card8.addEventListener('click', function () {
+    card8.classList.toggle('is-flipped');
+  });
+});
+/******/ })()
+;
